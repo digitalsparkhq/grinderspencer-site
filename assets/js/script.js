a3d4eddd7407c3823b7b3de7a1508446290c2edd
digitalsparkhq <biswajit@digitalsparkhq.com>
2025-10-05 05:44:23 +0530

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+// Navbar scroll effect
+window.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// Counter effect
+const counters = document.querySelectorAll('.counter');
+const speed = 200;
+
+const animateCounters = () => {
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+
+      const increment = target / speed;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCount, 20);
+      } else {
+        counter.innerText = target + "+";
+      }
+    };
+    updateCount();
+  });
+};
+
+let statsShown = false;
+window.addEventListener('scroll', () => {
+  const statsSection = document.querySelector('#stats');
+  if (statsSection && !statsShown) {
+    const sectionPos = statsSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight;
+    if (sectionPos < screenPos) {
+      animateCounters();
+      statsShown = true;
+    }
+  }
+});
